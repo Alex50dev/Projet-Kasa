cc107a9bcc52af8b4300349a633cc0b2560a1963
Alexis Levesque <alexis.levesque.dark@gmail.com>
2025-01-07 17:36:34 +0100

    Mise en place du SASS

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AppRouter from './components/Router';  // Importer le composant Router
+import "./styles/main.scss";
 
 function App() {
   return <AppRouter />; // Utiliser le routeur ici
