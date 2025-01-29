7fde722e714722dad9fc7e34fecb0df784ec78f8
Alexis Levesque <alexis.levesque.dark@gmail.com>
2025-01-29 16:02:40 +0100

    mise en forme de la page erreur 404 avec le scss

--- my-react-app/src/pages/NotFound.jsx
+++ my-react-app/src/pages/NotFound.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Pages/_NotFound.scss"; // Assure-toi d'avoir ce fichier
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>Page non trouvée</h1>
-      <p>La page que vous cherchez n'existe pas.</p>
+    <div className="notfound-container">
+      <h1 className="notfound-404">404</h1>
+      <p className="notfound-text">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link to="/" className="notfound-link">
+        Retourner sur la page d'accueil
+      </Link>
     </div>
   );
 };
